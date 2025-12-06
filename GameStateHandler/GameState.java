ed00daa24eeede7c20ecaf59bf23c96b38b23d26
User <user@example.com>
2025-12-06 15:11:00 +0530

    Main code

---
+++ GameStateHandler/GameState.java
@@ -0,0 +1,10 @@
+package GameStateHandler;
+
+import GameStateHandler.Context.GameContext;
+import Utility.Player;
+
+public interface GameState {
+    void next(GameContext context, Player player, boolean hasWon);
+
+    boolean isGameOver();
+}
--- main.java
+++ main.java
@@ -1,5 +1,14 @@
-public class main {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import Controller.GameController.TicTacToeGame;
+import PlayerStrategies.ConcreteStrategies.HumanPlayerStrategy;
+import PlayerStrategies.PlayerStrategy;
+
+// The main method serves as the entry point for the Tic-Tac-Toe game
+// application. It initializes the player strategies and starts the game.
+public class Main {
+    public static void main(String[] args) {
+        PlayerStrategy playerXStrategy = new HumanPlayerStrategy("Player X");
+        PlayerStrategy playerOStrategy = new HumanPlayerStrategy("Player O");
+        TicTacToeGame game = new TicTacToeGame(playerXStrategy, playerOStrategy, 3, 3);
+        game.play();
     }
 }

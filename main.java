660aa7d3a07be584092a81f1dfff8f4d90ac3862
User <user@example.com>
2025-12-06 15:50:51 +0530

    issue resolved

--- main.java
+++ main.java
@@ -2,8 +2,6 @@ import Controller.GameController.TicTacToeGame;
 import PlayerStrategies.ConcreteStrategies.HumanPlayerStrategy;
 import PlayerStrategies.PlayerStrategy;
 
-// The main method serves as the entry point for the Tic-Tac-Toe game
-// application. It initializes the player strategies and starts the game.
 public class Main {
     public static void main(String[] args) {
         PlayerStrategy playerXStrategy = new HumanPlayerStrategy("Player X");
